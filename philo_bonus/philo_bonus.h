37e09e4eef310a45897e5754571ae928706ac694
khk811 <hyunkyungkim811@gmail.com>
2022-05-27 11:59:31 +0900

    philo_bonus start

---
+++ philo_bonus/philo_bonus.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunkkim <hyunkkim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/27 11:53:17 by hyunkkim          #+#    #+#             */
+/*   Updated: 2022/05/27 11:56:50 by hyunkkim         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_BONUS_H
+# define PHILO_BONUS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <semaphore.h>
+# include <signal.h>
+
+#endif
