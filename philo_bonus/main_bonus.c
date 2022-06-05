f98fa034da9650871e7a28f3204ad3a3bd422a1f
khk811 <hyunkyungkim811@gmail.com>
2022-06-05 22:37:21 +0900

    separate time related func

--- philo_bonus/main_bonus.c
+++ philo_bonus/main_bonus.c
@@ -12,23 +12,6 @@
 
 #include "philo_bonus.h"
 
-size_t	get_milisecond(int sec, int usec)
-{
-	return ((sec * 1000) + (usec / 1000));
-}
-
-size_t	make_timestamp(struct timeval *start)
-{
-	struct timeval	curr;
-	size_t			curr_time;
-	size_t			start_time;
-
-	gettimeofday(&curr, NULL);
-	curr_time = get_milisecond(curr.tv_sec, curr.tv_usec);
-	start_time = get_milisecond(start->tv_sec, start->tv_usec);
-	return (curr_time - start_time);
-}
-
 void	print_statement(t_philo	*philo, char *s)
 {
 	sem_wait(philo->print);
---
+++ philo_bonus/time_bonus.c
@@ -0,0 +1,18 @@
+#include "philo_bonus.h"
+
+size_t	get_milisecond(int sec, int usec)
+{
+	return ((sec * 1000) + (usec / 1000));
+}
+
+size_t	make_timestamp(struct timeval *start)
+{
+	struct timeval	curr;
+	size_t			curr_time;
+	size_t			start_time;
+
+	gettimeofday(&curr, NULL);
+	curr_time = get_milisecond(curr.tv_sec, curr.tv_usec);
+	start_time = get_milisecond(start->tv_sec, start->tv_usec);
+	return (curr_time - start_time);
+}
